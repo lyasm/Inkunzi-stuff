3ae36264152a13082dc497c3d99f1c84fe9b4f27
lyasm <example1.beispiel@gmail.com>
2024-01-03 15:42:33 +0100

    preperations for tech tree

--- script.js
+++ script.js
@@ -52,3 +52,34 @@ document.addEventListener('DOMContentLoaded', function() {
 				armyData = armyJsonData;
 			})
 			.catch(error => console.error('Error loading army JSON:', error));
+
+		// Function to filter nations based on user input
+		function filterNations() {
+			const input = document.getElementById('nationDropdown').value.toLowerCase();
+			const filteredNations = Object.keys(data).filter(nationName => nationName.toLowerCase().includes(input));
+
+			// Update the datalist with filtered nations
+			const nationList = document.getElementById('nationList');
+			nationList.innerHTML = '';
+			filteredNations.forEach(nationName => {
+				const option = document.createElement('option');
+				option.value = nationName;
+				nationList.appendChild(option);
+			});
+		    readyDisplayNationDetails();
+		}
+
+function readyDisplayNationDetails() {
+			const selectedNationName = document.getElementById('nationDropdown').value;
+			const nationDetails = document.getElementById('nationDetails');
+			const armyDetails = document.getElementById('armyDetails');
+
+			// Find the selected nation in the JSON data
+    const selectedNationData = data[selectedNationName];
+    displayNationDetails(selectedNationData);
+			// Display the details in the HTML
+			// nationDetails.innerHTML = '<pre>' + JSON.stringify(selectedNationData, null, 2) + '</pre>';
+		}
+function displayNationDetails(nationData) {
+    
+}
